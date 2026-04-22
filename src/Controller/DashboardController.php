4e75ffb337ab8c9e1910d471b7078d289e6d6b5d
WakaTP-B <courcelles.amaury@orange.fr>
2026-04-22 11:46:27 +0200

    done

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
         ]);
     }
 }
+    
