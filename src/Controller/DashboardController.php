ac3872f7e04a7b8144bc96b15bf381f0aa6ad24d
WakaTP-B <courcelles.amaury@orange.fr>
2026-04-23 21:49:04 +0200

    swap

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Repository\TaskRepository;
+
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(TaskRepository $taskRepository): Response
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'tasks' => $taskRepository->findAll(),
         ]);
     }
 }
