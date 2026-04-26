9680d4ebcb8fc6dbd1e447eee712acf8ece185b3
WakaTP-B <courcelles.amaury@orange.fr>
2026-04-27 00:32:25 +0200

    pin done

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,13 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\User;
-use App\Enum\TaskStatus;
 use App\Repository\TaskRepository;
-use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -20,31 +17,8 @@ final class DashboardController extends AbstractController
     #[Route(['/dashboard', '/'], name: 'app_dashboard', methods: ['GET'])]
     public function index(
         #[CurrentUser] User $user,
-        Request $request,
-        TaskRepository $taskRepository,
-        EntityManagerInterface $entityManager
+        TaskRepository $taskRepository
     ): Response {
-
-        $justToggledId = $request->getSession()->get('just_toggled_task_id');
-        $request->getSession()->remove('just_toggled_task_id');
-
-        $completedTasks = $taskRepository->findBy([
-            'User' => $user,
-            'status' => TaskStatus::COMPLETED,
-        ]);
-
-        $hasChanges = false;
-        foreach ($completedTasks as $task) {
-            if ($task->getId() !== $justToggledId) {
-                $task->setStatus(TaskStatus::ARCHIVED);
-                $hasChanges = true;
-            }
-        }
-
-        if ($hasChanges) {
-            $entityManager->flush();
-        }
-
         $tasks = $taskRepository->findTasksSorted($user);
 
         return $this->render('dashboard/index.html.twig', [
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -51,9 +51,8 @@ final class TaskController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/done', name: 'app_task_done', methods: ['POST'])]
-    public function done(
-        Request $request,
+    #[Route('/{id}/status', name: 'app_task_status', methods: ['POST'])]
+    public function status(
         Task $task,
         EntityManagerInterface $entityManager,
         #[CurrentUser] User $user
@@ -65,72 +64,28 @@ final class TaskController extends AbstractController
         if ($task->getStatus() === TaskStatus::PENDING) {
             $task->setStatus(TaskStatus::COMPLETED);
         } elseif ($task->getStatus() === TaskStatus::COMPLETED) {
+            $task->setStatus(TaskStatus::ARCHIVED);
+        } elseif ($task->getStatus() === TaskStatus::ARCHIVED) {
             $task->setStatus(TaskStatus::PENDING);
         }
 
         $entityManager->flush();
 
-        $request->getSession()->set('just_toggled_task_id', $task->getId());
-
         return $this->redirectToRoute('app_dashboard');
     }
 
-    // #[Route('/{id}', name: 'app_task_show', methods: ['GET'])]
-    // public function show(Task $task, #[CurrentUser] User $user): Response
-    // {
-    //     if ($task->getUser() !== $user) {
-    //         throw $this->createAccessDeniedException("Utilisateur invalide");
-    //     }
+    #[Route('/{id}/pin', name: 'app_task_pin', methods: ['POST'])]
+    public function pin(
+        Task $task,
+        EntityManagerInterface $entityManager,
+        #[CurrentUser] User $user
+    ): Response {
+        if ($task->getUser() !== $user) {
+            throw $this->createAccessDeniedException('Accès refusé.');
+        }
+        $task->setIsPinned(!$task->isPinned());
+        $entityManager->flush();
 
-    //     return $this->render('task/show.html.twig', [
-    //         'task' => $task,
-    //     ]);
-    // }
-
-    // #[Route('/{id}/edit', name: 'app_task_edit', methods: ['GET', 'POST'])]
-    // public function edit(
-    //     Request $request,
-    //     Task $task,
-    //     EntityManagerInterface $entityManager,
-    //     #[CurrentUser] User $user
-    // ): Response {
-    //     if ($task->getUser() !== $user) {
-    //         throw $this->createAccessDeniedException('Utilisateur invalide');
-    //     }
-
-    //     $form = $this->createForm(TaskType::class, $task, [
-    //         'user' => $user,
-    //     ]);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->flush();
-
-    //         return $this->redirectToRoute('app_dashboard');
-    //     }
-
-    //     return $this->render('task/edit.html.twig', [
-    //         'task' => $task,
-    //         'form' => $form,
-    //     ]);
-    // }
-
-    // #[Route('/{id}', name: 'app_task_delete', methods: ['POST'])]
-    // public function delete(
-    //     Request $request,
-    //     Task $task,
-    //     EntityManagerInterface $entityManager,
-    //     #[CurrentUser] User $user
-    // ): Response {
-    //     if ($task->getUser() !== $user) {
-    //         throw $this->createAccessDeniedException('Utilisateur invalide');
-    //     }
-
-    //     if ($this->isCsrfTokenValid('delete' . $task->getId(), $request->getPayload()->getString('_token'))) {
-    //         $entityManager->remove($task);
-    //         $entityManager->flush();
-    //     }
-
-    //     return $this->redirectToRoute('app_dashboard');
-    // }
+        return $this->redirectToRoute('app_dashboard');
+    }
 }
